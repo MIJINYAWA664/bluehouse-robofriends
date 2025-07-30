73c429d3b0ec72de10750ef3123c42c56f0abb6d
MIJINYAWA664 <mijinyawadau664@gmail.com>
2025-07-30 13:16:22 +0100

    new

---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import  Card from "./Card"
+
+const CardList =({robots}) => (
+    <div>
+        {robots.map(({id,name,email}) =>(
+            <Card
+            key={id}
+            id={id}
+            name={name}
+            email={email}
+            />
+
+        ))}
+    </div>
+);
+
+export default CardList;
